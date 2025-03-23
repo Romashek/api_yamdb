c9b9c9c8dbe43eec18827e36bc3d44000bf2f121
Антонов Роман <ars2015evel@gmail.com>
2025-03-23 21:03:57 +0300

    Финал 15-го спринта

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
+from django.contrib.auth import views
 from .views import (TitleViewSet, ReviewViewSet, CommentViewSet,
                     CategoryViewSet, GenreViewSet, UserViewSet)
 
@@ -19,5 +20,6 @@ v1_router.register('users', UserViewSet, basename='users')
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
     path('v1/', include('djoser.urls.jwt')),
-    path('auth/', include('django.contrib.auth.urls')),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
